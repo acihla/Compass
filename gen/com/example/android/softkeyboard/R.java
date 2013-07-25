f98e6f61afed4b54764b56108652024ed8158ce9
Tim Brown <tbrown126@Tims-MacBook-Pro-2.local>
2013-07-24 20:08:31 -0700

    for updated databuidler

--- gen/com/example/android/softkeyboard/R.java
+++ gen/com/example/android/softkeyboard/R.java
@@ -26,28 +26,29 @@ public final class R {
         public static final int compass=0x7f020001;
         public static final int compasslower=0x7f020002;
         public static final int compassnumbers=0x7f020003;
-        public static final int compassupper=0x7f020004;
-        public static final int eb=0x7f020005;
-        public static final int eg=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int icon_en_gb=0x7f020008;
-        public static final int icon_en_us=0x7f020009;
-        public static final int nb=0x7f02000a;
-        public static final int neg=0x7f02000b;
-        public static final int ng=0x7f02000c;
-        public static final int nwg=0x7f02000d;
-        public static final int sb=0x7f02000e;
-        public static final int seg=0x7f02000f;
-        public static final int sg=0x7f020010;
-        public static final int swg=0x7f020011;
-        public static final int sym_keyboard_delete=0x7f020012;
-        public static final int sym_keyboard_done=0x7f020013;
-        public static final int sym_keyboard_return=0x7f020014;
-        public static final int sym_keyboard_search=0x7f020015;
-        public static final int sym_keyboard_shift=0x7f020016;
-        public static final int sym_keyboard_space=0x7f020017;
-        public static final int wb=0x7f020018;
-        public static final int wg=0x7f020019;
+        public static final int compasssnumbers=0x7f020004;
+        public static final int compassupper=0x7f020005;
+        public static final int eb=0x7f020006;
+        public static final int eg=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int icon_en_gb=0x7f020009;
+        public static final int icon_en_us=0x7f02000a;
+        public static final int nb=0x7f02000b;
+        public static final int neg=0x7f02000c;
+        public static final int ng=0x7f02000d;
+        public static final int nwg=0x7f02000e;
+        public static final int sb=0x7f02000f;
+        public static final int seg=0x7f020010;
+        public static final int sg=0x7f020011;
+        public static final int swg=0x7f020012;
+        public static final int sym_keyboard_delete=0x7f020013;
+        public static final int sym_keyboard_done=0x7f020014;
+        public static final int sym_keyboard_return=0x7f020015;
+        public static final int sym_keyboard_search=0x7f020016;
+        public static final int sym_keyboard_shift=0x7f020017;
+        public static final int sym_keyboard_space=0x7f020018;
+        public static final int wb=0x7f020019;
+        public static final int wg=0x7f02001a;
     }
     public static final class id {
         public static final int compass=0x7f0a0000;
@@ -56,13 +57,14 @@ public final class R {
         public static final int compass=0x7f030000;
     }
     public static final class raw {
-        public static final int book1=0x7f050000;
-        public static final int keygonsequenceoutput=0x7f050001;
-        public static final int keygonsequenceoutput10k1=0x7f050002;
-        public static final int keygonsequenceoutput10k2=0x7f050003;
-        public static final int keygonsequenceoutput10k3=0x7f050004;
-        public static final int keygonsequenceoutput10k4=0x7f050005;
-        public static final int keygonsequenceoutput10k5=0x7f050006;
+        public static final int basics100k=0x7f050000;
+        public static final int book1=0x7f050001;
+        public static final int keygonsequenceoutput=0x7f050002;
+        public static final int keygonsequenceoutput10k1=0x7f050003;
+        public static final int keygonsequenceoutput10k2=0x7f050004;
+        public static final int keygonsequenceoutput10k3=0x7f050005;
+        public static final int keygonsequenceoutput10k4=0x7f050006;
+        public static final int keygonsequenceoutput10k5=0x7f050007;
     }
     public static final class string {
         public static final int general_category=0x7f08000a;
